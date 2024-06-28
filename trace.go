37658859ff5f2fb2cf3cf93559ed81100ef056b0
kakysha <kakysha@me.com>
2024-06-28 14:03:49 +0300

    feat: mark traces with tasks

--- trace.go
+++ trace.go
@@ -1,8 +1,10 @@
 package metrics
 
 import (
+	"context"
 	"fmt"
 	"os"
+	rtrace "runtime/trace"
 	"time"
 
 	"golang.org/x/exp/trace"
@@ -31,10 +33,12 @@ func NewTraceRecorder(period, snapshotThreshold time.Duration, bufferSizeBytes i
 // then it flushes recorder trace buffer to file
 func (tr TraceRecorder) StartRegion(tagName, tagValue string) (stopRegion func() error) {
 	start := time.Now()
+	_, task := rtrace.NewTask(context.Background(), fmt.Sprintf("%s=%s", tagName, tagValue))
 	return func() error {
+		task.End()
 		if time.Since(start) > tr.snapshotThreshold { // snapshot trace
 			fileName := fmt.Sprintf("trace-%s-%s-%d.out", tagName, tagValue, start.Unix())
-			fmt.Printf("::: writing Trace Recorder snapshot to file %s:::\n", fileName)
+			fmt.Printf("::: writing Trace Recorder snapshot to file %s :::\n", fileName)
 			f, err := os.Create(fileName)
 			if err != nil {
 				return err
