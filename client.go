33c227e23f604b64293a8e334ab3dfc246b2c952
danidomi <daniel.alexandre.domingues@gmail.com>
2024-09-18 13:59:43 +0100

    feat(GEN-95): add datadog support feat(HELIX-1134): add mixpanel support

--- client.go
+++ client.go
@@ -1,16 +1,20 @@
 package metrics
 
 import (
-	"fmt"
+	"runtime"
 	"sync"
 	"time"
 
 	dogstatsd "github.com/DataDog/datadog-go/v5/statsd"
-	statsd "github.com/alexcesaro/statsd"
+	"github.com/alexcesaro/statsd"
+	"github.com/mixpanel/mixpanel-go"
 	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
 	ddotel "gopkg.in/DataDog/dd-trace-go.v1/ddtrace/opentelemetry"
+	"gopkg.in/DataDog/dd-trace-go.v1/profiler"
+
+	log "github.com/InjectiveLabs/suplog"
 )
 
 const (
@@ -25,8 +29,9 @@ var (
 	clientMux = new(sync.RWMutex)
 	config    *StatterConfig
 
-	traceProvider *ddotel.TracerProvider
-	tracer        trace.Tracer
+	traceProvider  *ddotel.TracerProvider
+	tracer         trace.Tracer
+	mixPanelClient *mixpanel.ApiClient
 )
 
 type StatterConfig struct {
@@ -35,10 +40,15 @@ type StatterConfig struct {
 	Agent                string        // telegraf/datadog
 	EnvName              string        // dev/test/staging/prod
 	HostName             string        // hostname
+	Version              string        // version
 	StuckFunctionTimeout time.Duration // stuck time
+	MockingThreshold     time.Duration // mocking threshold
 	MockingEnabled       bool          // whether to enable mock statter, which only produce logs
 	Disabled             bool          // whether to disable metrics completely
 	TracingEnabled       bool          // whether DataDog tracing should be enabled (via OpenTelemetry)
+	ProfilingEnabled     bool          // whether Datadog profiling should be enabled
+	MixPanelEnabled      bool          // whether MixPanel should be enabled
+	MixPanelProjectToken string        // MixPanel project token
 }
 
 func (m *StatterConfig) BaseTags() []string {
@@ -82,35 +92,15 @@ func Close() {
 	if client == nil {
 		return
 	}
-	if traceProvider != nil {
-		traceProvider.Shutdown()
-	}
 	client.Close()
 }
 
-func Disable() {
-	config = checkConfig(nil)
-	clientMux.Lock()
-	client = newMockStatter(true)
-	clientMux.Unlock()
-	tracer = nil
-}
-
-func InitWithConfig(cfg *StatterConfig) error {
-	return Init(cfg.Addr, cfg.Prefix, cfg)
-}
-
 func Init(addr string, prefix string, cfg *StatterConfig) error {
-	if cfg.Disabled {
-		Disable()
-		return nil
-	}
-
 	config = checkConfig(cfg)
 	if config.MockingEnabled {
 		// init a mock statter instead of real statsd client
 		clientMux.Lock()
-		client = newMockStatter(false)
+		client = newMockStatter(cfg)
 		clientMux.Unlock()
 		return nil
 	}
@@ -150,12 +140,51 @@ func Init(addr string, prefix string, cfg *StatterConfig) error {
 	clientMux.Unlock()
 
 	// OpenTelemetry tracing via DataDog provider
-	if cfg.TracingEnabled {
+	if cfg.Agent == DatadogAgent && cfg.TracingEnabled {
 		traceProvider = ddotel.NewTracerProvider()
 		otel.SetTracerProvider(traceProvider)
 		tracer = otel.Tracer("")
 	}
 
+	if cfg.Agent == DatadogAgent && cfg.ProfilingEnabled {
+		err = setupProfiler(cfg)
+		if err != nil {
+			return err
+		}
+	}
+
+	if cfg.MixPanelEnabled {
+		StartMixPanel(cfg.MixPanelProjectToken)
+	}
+
+	return nil
+}
+
+func StartMixPanel(projectToken string) {
+	clientMux.Lock()
+	defer clientMux.Unlock()
+	mixPanelClient = mixpanel.NewApiClient(projectToken)
+}
+
+func setupProfiler(cfg *StatterConfig) error {
+	runtime.SetMutexProfileFraction(5)
+	runtime.SetBlockProfileRate(5)
+
+	err := profiler.Start(
+		profiler.WithService(cfg.Prefix),
+		profiler.WithEnv(cfg.EnvName),
+		profiler.WithHostname(cfg.HostName),
+		profiler.WithVersion(cfg.Version),
+		profiler.WithProfileTypes(
+			profiler.CPUProfile,
+			profiler.HeapProfile,
+			profiler.BlockProfile,
+			profiler.MutexProfile,
+		),
+	)
+	if err != nil {
+		return errors.Wrap(err, "profiler start failed")
+	}
 	return nil
 }
 
@@ -173,44 +202,89 @@ func checkConfig(cfg *StatterConfig) *StatterConfig {
 }
 
 func errHandler(err error) {
-	fmt.Printf("statsd error, err: %v\n", err)
+	log.WithError(err).Errorln("statsd error")
 }
 
-func newMockStatter(noop bool) Statter {
-	return &mockStatter{}
+func newMockStatter(cfg *StatterConfig) Statter {
+	return &mockStatter{
+		l: log.WithFields(log.Fields{
+			"module": "mock_statter",
+		}),
+		threshold: cfg.MockingThreshold,
+	}
 }
 
 type mockStatter struct {
+	l         log.Logger
+	threshold time.Duration
 }
 
 func (s *mockStatter) Count(name string, value int64, tags []string, rate float64) error {
+	if s.threshold > 0 {
+		return nil
+	}
+	s.l.WithFields(s.withTagFields(tags)).Debugf("Count %s: %v", name, value)
 	return nil
 }
 
 func (s *mockStatter) Incr(name string, tags []string, rate float64) error {
+	if s.threshold > 0 {
+		return nil
+	}
+	s.l.WithFields(s.withTagFields(tags)).Debugf("Incr %s", name)
 	return nil
 }
 
 func (s *mockStatter) Decr(name string, tags []string, rate float64) error {
+	if s.threshold > 0 {
+		return nil
+	}
+	s.l.WithFields(s.withTagFields(tags)).Debugf("Decr %s", name)
 	return nil
 }
 
 func (s *mockStatter) Gauge(name string, value float64, tags []string, rate float64) error {
+	if s.threshold > 0 {
+		return nil
+	}
+	s.l.WithFields(s.withTagFields(tags)).Debugf("Gauge %s: %v", name, value)
 	return nil
 }
 
 func (s *mockStatter) Timing(name string, value time.Duration, tags []string, rate float64) error {
+	if value > s.threshold {
+		s.l.WithFields(s.withTagFields(tags)).Debugf("Timing %s: %v", name, value)
+	}
 	return nil
 }
 
 func (s *mockStatter) Histogram(name string, value float64, tags []string, rate float64) error {
+	if value > float64(s.threshold.Milliseconds()) {
+		s.l.WithFields(s.withTagFields(tags)).Debugf("Histogram %s: %v", name, value)
+	}
 	return nil
 }
 
 func (s *mockStatter) Unique(bucket string, value string) error {
+	if s.threshold > 0 {
+		return nil
+	}
+	s.l.Debugf("Unique %s: %v", bucket, value)
 	return nil
 }
 
 func (s *mockStatter) Close() error {
+	s.l.Debugf("closed at %s", time.Now())
 	return nil
+}
+
+func (s *mockStatter) withTagFields(tags []string) log.Fields {
+	fields := make(log.Fields)
+	for i := 0; i < len(tags); i += 2 {
+		if i+1 >= len(tags) { // protect against odd number of tags
+			break
+		}
+		fields[tags[i]] = tags[i+1]
+	}
+	return fields
 }
--- reports.go
+++ reports.go
@@ -1,5 +1,12 @@
 package metrics
 
+import (
+	"strconv"
+	"time"
+
+	"golang.org/x/exp/constraints"
+)
+
 func CustomReport(reportFn func(s Statter, tagSpec []string), tags ...Tags) {
 	clientMux.RLock()
 	defer clientMux.RUnlock()
@@ -33,4 +40,43 @@ func IndexPriceUpdatesBatchSubmitted(size int, tags ...Tags) {
 	CustomReport(func(s Statter, tagSpec []string) {
 		s.Count("events.set_price_batch.size", int64(size), tagSpec, 1)
 	})
+}
+
+func Counter(metric string, value int64, tags ...Tags) {
+	CustomReport(func(s Statter, tagSpec []string) {
+		s.Count(metric, value, tagSpec, 1)
+	}, tags...)
+}
+
+func CounterPositive[T constraints.Integer](metric string, value T, tags ...Tags) {
+	if value > 0 {
+		Counter(metric, int64(value), tags...)
+	}
+}
+
+func Incr(metric string, tags ...Tags) {
+	Counter(metric, 1, tags...)
+}
+
+func Timer(metric string, value time.Duration, tags ...Tags) {
+	CustomReport(func(s Statter, tagSpec []string) {
+		s.Timing(metric, value, tagSpec, 1)
+	}, tags...)
+}
+
+func Gauge(metric string, value float64, tags ...Tags) {
+	CustomReport(func(s Statter, tagSpec []string) {
+		s.Gauge(metric, value, tagSpec, 1)
+	}, tags...)
+}
+
+func BoolTag(b bool) string {
+	if b {
+		return "true"
+	}
+	return "false"
+}
+
+func IntTag[T constraints.Integer](i T) string {
+	return strconv.Itoa(int(i))
 }
