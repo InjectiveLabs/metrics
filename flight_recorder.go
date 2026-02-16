8605f15d9f1467449065469d997357f54f2c5ffc
kakysha <kakysha@me.com>
2026-02-16 13:40:55 +0200

    feat: flight recorder remove Once

--- flight_recorder.go
+++ flight_recorder.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"runtime/trace"
-	"sync"
 	"time"
 )
 
@@ -14,10 +13,6 @@ type TraceRecorder struct {
 
 	snapshotThreshold time.Duration
 }
-
-var (
-	once sync.Once
-)
 
 // NewTraceRecorder creates new trace flight recorder that will continuously record latest execution trace in a circullar buffer
 // and snapshot it to file only if region takes more than snapshotThreshold
@@ -51,18 +46,16 @@ func (tr TraceRecorder) StartRegion(tagName, tagValue string) (stopRegion func()
 }
 
 func (tr TraceRecorder) snapshotToFile(tagName, tagValue string) {
-	once.Do(func() {
-		fileName := fmt.Sprintf("trace-%s-%s-%d.out", tagName, tagValue, time.Now().Unix())
-		fmt.Printf("::: writing Flight Recorder snapshot to file %s :::\n", fileName)
-		f, err := os.Create(fileName)
-		if err != nil {
-			fmt.Printf("error creating file: %s", err)
-			return
-		}
-		_, err = tr.WriteTo(f)
-		if err != nil {
-			fmt.Printf("error writing to file file: %s", err)
-			return
-		}
-	})
+	fileName := fmt.Sprintf("trace-%s-%s-%d.out", tagName, tagValue, time.Now().Unix())
+	fmt.Printf("::: writing Flight Recorder snapshot to file %s :::\n", fileName)
+	f, err := os.Create(fileName)
+	if err != nil {
+		fmt.Printf("error creating file: %s", err)
+		return
+	}
+	_, err = tr.WriteTo(f)
+	if err != nil {
+		fmt.Printf("error writing to file file: %s", err)
+		return
+	}
 }
